f6116571e380a9f7a57df51ab723e74d72830725
OmarInCS <omarincs@gmail.com>
2021-12-23 10:23:33 +0300

    add markers

--- lib/gmap_app/gmap_screen.dart
+++ lib/gmap_app/gmap_screen.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -15,6 +17,7 @@ class _GMapScreenState extends State<GMapScreen> {
   late Future<Position> userLocation;
   late GoogleMapController mapController;
   Set<Marker> myLocations = Set();
+  Random randGen = Random();
 
   Future<Position> getUserLocation() async {
     bool serviceEnabled;
@@ -53,6 +56,25 @@ class _GMapScreenState extends State<GMapScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Map"),
+        actions: [
+          TextButton(
+            child: Text("Random",
+              style: TextStyle(
+                  color: Colors.white
+              ),
+            ),
+            onPressed: () {
+              mapController.animateCamera(
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: LatLng(randGen.nextDouble() * 100, randGen.nextDouble() * 100),
+                    zoom: 8
+                  )
+                )
+              );
+            },
+          )
+        ],
       ),
       body: FutureBuilder(
         future: userLocation,
@@ -76,12 +98,23 @@ class _GMapScreenState extends State<GMapScreen> {
                 ),
                 Expanded(
                   child: GoogleMap(
+                    mapType: MapType.normal,
                     initialCameraPosition: CameraPosition(
                       target: LatLng(snapshot.data!.latitude, snapshot.data!.longitude),
                       zoom: 16
                     ),
                     markers: myLocations,
                     onMapCreated: (controller) => mapController = controller,
+                    onLongPress: (position) {
+                      myLocations.add(Marker(
+                          markerId: MarkerId(myLocations.length.toString()),
+                          position: LatLng(position.latitude, position.longitude),
+                          infoWindow: InfoWindow(title: "New Location")
+                      ));
+                      setState(() {
+
+                      });
+                    },
                   ),
                 )
               ],
